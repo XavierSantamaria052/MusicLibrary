a0d96dc6ab1e652f71898b3280ba5a19e2f19dc5
Xavier Santamaria <xsantamariatr@gmail.com>
2026-05-06 22:10:50 -0600

    Delete Documents/XAVIER EBAC/TRABAJOS directory

--- Documents/XAVIER EBAC/TRABAJOS/ebac-backend-java-master/src/main/java/com/ebac/modulo37/componentes/ClaseModelo.java
+++
@@ -1,9 +0,0 @@
-package com.ebac.modulo37.componentes;
-
-public class ClaseModelo {
-
-    public String getElementoPorId(int id) {
-        //Simular una llamada a la base de datos
-        return "El elemento con id " + id + " fue encontrado";
-    }
-}
